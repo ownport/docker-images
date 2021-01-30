d3b07d03802736ec6726aa1ac30aa0ac24a5885a
ownport <ownport@gmail.com>
2021-01-30 23:36:45 +0200

    refact: manage.sh script

--- manage.sh
+++ manage.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
 set -eu
+# GitLab Docker Registry
+GITLAB_DOCKER_REGISTRY="registry.gitlab.com"
+# GitLab Group
+GITLAB_GROUP=ownport
+# GitLab Project
+GITLAB_PROJECT=docker-images
+
 
 case ${1} in
     help)
         echo 'Available options:'
-        echo ' update-base-images   - update base images: alpine, ubuntu, ..'
-        echo ' build                - build docker images'
-        echo ' console              - run console for specific image'
+        echo ' update-base-images       - update base images: alpine, ubuntu, ..'
+        echo ' build                    - build docker image'
+        echo ' test                     - run tests for docker image'
+        echo ' remove                   - remove docker image'
+        echo ' publish-to-gl-registry   - publish docker image to GitLab Registry'
+        echo ' console                  - run console for specific image'
         ;;
     update-base-images)
         echo "[INFO] Updating base images" && \
@@ -27,7 +37,76 @@ case ${1} in
             echo "[INFO] Building image: ${DOCKERFILE_PATH}"
             cd ${DOCKERFILE_PATH}
             source metadata
-            docker build -t ownport/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} .
+
+            # Set GitLab Docker Image
+            GITLAB_DOCKER_IMAGE=${GITLAB_DOCKER_REGISTRY}/${GITLAB_GROUP}/${GITLAB_PROJECT}/${DOCKER_IMAGE_NAME}
+            
+            # Use Docker caching
+            docker pull ${GITLAB_DOCKER_IMAGE}:latest || true
+    
+            # Build docker image
+            docker build --cache-from ${GITLAB_DOCKER_IMAGE}:latest \
+                --tag ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} \
+                --tag ${GITLAB_DOCKER_IMAGE}:latest .
+        }
+        ;;
+    test)
+        shift
+        DOCKERFILE_PATH=${1:-}
+
+        [ -z "${DOCKERFILE_PATH}" ] && {
+            echo "[ERROR] Please specify the image name by 'image' parameter"
+            exit 1
+        } || {
+            echo "[INFO] Tesing image: ${DOCKERFILE_PATH}"
+            cd ${DOCKERFILE_PATH}
+            source metadata
+
+            # Set GitLab Docker Image
+            GITLAB_DOCKER_IMAGE=${GITLAB_DOCKER_REGISTRY}/${GITLAB_GROUP}/${GITLAB_PROJECT}/${DOCKER_IMAGE_NAME}
+
+            # Run tests for docker image
+            docker run --rm -v $(pwd)/tests:/tests \
+		            ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} /tests/run-tests.sh
+        }
+        ;;
+    remove)
+        shift
+        DOCKERFILE_PATH=${1:-}
+
+        [ -z "${DOCKERFILE_PATH}" ] && {
+            echo "[ERROR] Please specify the image name by 'image' parameter"
+            exit 1
+        } || {
+            echo "[INFO] Removing image from path: ${DOCKERFILE_PATH}"
+            cd ${DOCKERFILE_PATH}
+            source metadata
+
+            # Set GitLab Docker Image
+            GITLAB_DOCKER_IMAGE=${GITLAB_DOCKER_REGISTRY}/${GITLAB_GROUP}/${GITLAB_PROJECT}/${DOCKER_IMAGE_NAME}
+
+            # Remove docker images
+            docker image rm ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} ${GITLAB_DOCKER_IMAGE}:latest
+        }
+        ;;
+    publish-to-gitlab-registry)
+        shift
+        DOCKERFILE_PATH=${1:-}
+
+        [ -z "${DOCKERFILE_PATH}" ] && {
+            echo "[ERROR] Please specify the image name by 'image' parameter"
+            exit 1
+        } || {
+            echo "[INFO] Publish image from path: ${DOCKERFILE_PATH} to GitLab Registry: ${GITLAB_DOCKER_REGISTRY}"
+            cd ${DOCKERFILE_PATH}
+            source metadata
+
+            # Set GitLab Docker Image
+            GITLAB_DOCKER_IMAGE=${GITLAB_DOCKER_REGISTRY}/${GITLAB_GROUP}/${GITLAB_PROJECT}/${DOCKER_IMAGE_NAME}
+
+            # Push docker image(-s) to GitLab Docker Registry
+            docker push ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} 
+            docker push ${GITLAB_DOCKER_IMAGE}:latest
         }
         ;;
     console)
@@ -41,9 +120,15 @@ case ${1} in
             echo "[INFO] Running console for image: ${DOCKERFILE_PATH}"
             cd ${DOCKERFILE_PATH}
             source metadata
-            echo "[INFO] The image: ownport/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}, the container name: ${DOCKER_IMAGE_NAME}-console"
+
+            # Set GitLab Docker Image
+            GITLAB_DOCKER_IMAGE=${GITLAB_DOCKER_REGISTRY}/${GITLAB_GROUP}/${GITLAB_PROJECT}/${DOCKER_IMAGE_NAME}
+
+            echo "[INFO] The image: ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION}, the container name: ${DOCKER_IMAGE_NAME}-console"
+
+            # Open console
             docker run -ti --rm --name ${DOCKER_IMAGE_NAME}-console \
-                ownport/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} \
+                ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} \
                 ${DOCKER_IMAGE_SHELL}
         }
         ;;
@@ -55,5 +140,3 @@ case ${1} in
         fi
         ;;
 esac 
-
-
