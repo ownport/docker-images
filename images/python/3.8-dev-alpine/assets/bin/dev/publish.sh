21a7f783f316bf3a0e5f78b5335fe0a3d9f692de
ownport <ownport@gmail.com>
2021-01-31 14:49:01 +0200

    fix: dev scripts

--- images/python/3.8-dev-alpine/assets/bin/dev/publish.sh
+++ images/python/3.8-dev-alpine/assets/bin/dev/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "[INFO] Publishing package"
 python3 setup.py sdist bdist_wheel
--- images/python/3.8-dev-alpine/assets/bin/dev/run-pytest.sh
+++ images/python/3.8-dev-alpine/assets/bin/dev/run-pytest.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-/usr/sbin/dev/cleanup.sh pre-cleanup
+/usr/local/bin/dev/cleanup.sh pre-cleanup
 
 # In case of using local PyPi server use pip3 with the next options:
 #   --trusted-host pypi-server
@@ -10,11 +10,10 @@ set -e
 
 pip3 install -e . && \
     PYTHONDONTWRITEBYTECODE=1 pytest \
-    --cov=eventsflow \
     -p no:cacheprovider \
     --junitxml=report.xml \
     --cov-report=term-missing \
     --cov-config=.coveragerc
 
-/usr/sbin/dev/cleanup.sh post-cleanup
+/usr/local/bin/dev/cleanup.sh post-cleanup
 
