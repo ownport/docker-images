72a4b89cb5ce9e2e446ada11ba11d461ef69da5a
ownport <ownport@gmail.com>
2021-01-31 10:27:40 +0200

    refact: add test completion message

--- manage.sh
+++ manage.sh
@@ -68,7 +68,8 @@ case ${1} in
 
             # Run tests for docker image
             docker run --rm -v "$(pwd)/tests":/tests \
-		            ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} /tests/run-tests.sh && echo 'OK'
+		            ${GITLAB_DOCKER_IMAGE}:${DOCKER_IMAGE_VERSION} /tests/run-tests.sh && \
+            echo "[INFO] Tests were completed"
         }
         ;;
     remove)
