aa5919b6710fb5353491adfd6a388b5a15950ecf
ownport <ownport@gmail.com>
2021-01-31 08:15:00 +0200

    refact: remove versions

--- images/python/3.8-dev-alpine/assets/bin/install.sh
+++ images/python/3.8-dev-alpine/assets/bin/install.sh
@@ -8,8 +8,8 @@ deploy-utils.sh update
 # Installation
 deploy-utils.sh install-build-deps "py3-wheel"
 
-py3-utils.sh install "pylint==2.5.3"
-py3-utils.sh install "pytest==5.4.3 pytest-cov==2.10.0 pytest-xdist==1.33.0 pytest-benchmark==3.2.3"
+py3-utils.sh install "pylint"
+py3-utils.sh install "pytest pytest-cov pytest-xdist pytest-benchmark"
 
 # Cleanup procedure
 deploy-utils.sh cleanup
