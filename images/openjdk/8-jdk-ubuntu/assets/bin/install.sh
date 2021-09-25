32f62090df7c5819889b04233abadfd60a2281d9
ownport <ownport@gmail.com>
2021-09-25 09:55:31 +0300

    feat: openjdk:8-jdk-ubuntu init commit

---
+++ images/openjdk/8-jdk-ubuntu/assets/bin/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -eu
+
+# removing extra files
+cleanup() {
+
+    echo "[INFO] Cleaning extra files" && \
+        rm -rf \
+            /usr/share/X11 #\
+            # /usr/lib/jvm/java-11-openjdk-amd64/man \
+            # /usr/lib/jvm/java-11-openjdk-amd64/demo \
+            # /usr/lib/jvm/java-11-openjdk-amd64/jmods/java.desktop.* \
+            # /usr/lib/jvm/java-11-openjdk-amd64/jmods/jdk.unsupported.desktop.jmod
+}
+
+# Preparation
+deploy-utils.sh update
+
+# Installation
+deploy-utils.sh install "openjdk-8-jdk-headless"
+
+# Cleanup procedure
+cleanup
+deploy-utils.sh cleanup
+
+
+
+
