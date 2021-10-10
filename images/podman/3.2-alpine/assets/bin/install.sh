95246b8cc528035670f2444298ecf1b603936bfa
ownport <ownport@gmail.com>
2021-10-10 12:30:03 +0300

    feat: use podman version = 3.2.3-r1

--- images/podman/3.2-alpine/assets/bin/install.sh
+++ images/podman/3.2-alpine/assets/bin/install.sh
@@ -6,7 +6,10 @@ set -eu
 deploy-utils.sh update
 
 # Installation
-deploy-utils.sh install "podman=3.2.3"
+deploy-utils.sh install "podman=3.2.3-r1"
+
+# Configure user and group
+deploy-utils.sh add-user-and-group 1000 podman 1000 podman
 
 # Cleanup procedure
 deploy-utils.sh cleanup
