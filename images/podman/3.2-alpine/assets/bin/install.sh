586af824ebf04f59dad1d59a6a410b6fa5848f88
ownport <ownport@gmail.com>
2021-10-10 13:03:06 +0300

    feat: add podman configs update

--- images/podman/3.2-alpine/assets/bin/install.sh
+++ images/podman/3.2-alpine/assets/bin/install.sh
@@ -8,6 +8,9 @@ deploy-utils.sh update
 # Installation
 deploy-utils.sh install "podman=3.2.3-r1"
 
+# Update podman configs
+mv /tmp/assets/conf/* /etc/containers/
+
 # Configure user and group
 deploy-utils.sh add-user-and-group 1000 podman 1000 podman
 
