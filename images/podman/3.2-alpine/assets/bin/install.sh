e3a718c1cb2df6cb57d3fbdda73b85decbee31d4
ownport <ownport@gmail.com>
2021-10-11 08:44:51 +0300

    feat: podman docker image

--- images/podman/3.2-alpine/assets/bin/install.sh
+++ images/podman/3.2-alpine/assets/bin/install.sh
@@ -8,11 +8,29 @@ deploy-utils.sh update
 # Installation
 deploy-utils.sh install "podman=3.2.3-r1"
 
-# Update podman configs
-mv /tmp/assets/conf/* /etc/containers/
-
 # Configure user and group
 deploy-utils.sh add-user-and-group 1000 podman 1000 podman
+
+# Since we can run rootfull and rootless containers with this image 
+# we create two volumes.  Rootfull Podman uses 
+#
+#   /var/lib/containers  
+#
+# for it’s container storage and rootless uses 
+#
+#   /home/podman/.local/share/containers.  
+#
+# Overlay over overlay is often denied by the kernel, so this creates 
+# non overlay volumes to be used within the container.
+echo "[INFO] Create podman directories" && \
+    mkdir -p \
+        /var/lib/containers \
+        /home/podman/.local/share/containers
+
+# Update podman configs
+echo "[INFO] Copy containers configs" && \
+    mv /tmp/assets/conf/* /etc/containers/
+
 
 # Cleanup procedure
 deploy-utils.sh cleanup
---
+++ images/podman/README.md
@@ -0,0 +1,9 @@
+# podman docker image
+
+https://podman.io/
+
+
+## References
+
+- How to use Podman inside of a container, https://www.redhat.com/sysadmin/podman-inside-container
+- https://www.atlantic.net/dedicated-server-hosting/how-to-install-and-use-podman-on-ubuntu-20-04/, https://www.atlantic.net/dedicated-server-hosting/how-to-install-and-use-podman-on-ubuntu-20-04/
