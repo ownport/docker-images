ac3a91c5a06ada8cc046b485a44ac66d778d0bcf
ownport <ownport@gmail.com>
2021-10-14 23:03:04 +0300

    test: add smoke tests for python:3.8-alpine

--- images/python/3.8-alpine/assets/bin/install.sh
+++ images/python/3.8-alpine/assets/bin/install.sh
@@ -11,6 +11,9 @@ deploy-utils.sh update
 # Installation
 deploy-utils.sh install "python3 py3-pip"
 
+echo "[INFO] Running smoke tests" && \
+    /tmp/assets/tests/smoke-tests.sh
+
 # Cleanup procedure
 deploy-utils.sh cleanup
 py3-utils.sh cleanup
