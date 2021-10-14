1d5ab6b33a0779904690ff6d6639149f7a5eb72f
ownport <ownport@gmail.com>
2021-10-14 20:53:14 +0000

    feature/101: migrate python docker images to kaniko builder

--- images/python/3.9-ubuntu/assets/bin/install.sh
+++ images/python/3.9-ubuntu/assets/bin/install.sh
@@ -9,8 +9,8 @@ echo "[INFO] Install py3-utils script" && \
     mv /tmp/assets/bin/py3-utils.sh /usr/local/bin/ && \
 
 # Installation
-deploy-utils.sh install "apt-utils"
-deploy-utils.sh install "python3"
+# deploy-utils.sh install "apt-utils"
+deploy-utils.sh install "python3 python3-pip"
 
 echo "[INFO] Running smoke tests" && \
     /tmp/assets/tests/smoke-tests.sh
