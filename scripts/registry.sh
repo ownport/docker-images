4a2f7dbf697d124fbfd9fc0aedc01a94ea86fabe
ownport <ownport@gmail.com>
2021-10-17 21:42:20 +0300

    feat: add list GitLab projects

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -3,6 +3,12 @@
 GITLAB_PROJECT="ownport/docker-images"
 
 URL_REPO_LIST="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT}/registry/repositories"
+URL_PROJECT_IDS="https://gitlab.com/api/v4/projects/"
+
+list_projects() {
+
+    curl --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" ${URL_PROJECT_IDS}
+}
 
 list_repos() {
 
