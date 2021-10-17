383082cb7696eba88307022966e8b419b1df8690
ownport <ownport@gmail.com>
2021-10-17 21:33:36 +0300

    feat: add script for getting repos list

---
+++ scripts/registry.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+GITLAB_PROJECT="docker-images"
+
+URL_REPO_LIST="https://gitlab.example.com/api/v4/projects/${GITLAB_PROJECT}/registry/repositories"
+
+list_repos() {
+
+    curl --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN} ${URL_REPO_LIST}" 
+
+}
+
+$@
