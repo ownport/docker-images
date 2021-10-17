a89b2e789dc71c7f0e24cd40278a1529bd4a00d3
ownport <ownport@gmail.com>
2021-10-17 22:57:32 +0300

    test: tag count is true for repos list

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -4,7 +4,7 @@
 GITLAB_PROJECT_ID="23580555"
 
 # URLs
-URL_REPO_LIST="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/registry/repositories"
+URL_REPO_LIST="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/registry/repositories?tag_count=true"
 
 list_repos() {
 
