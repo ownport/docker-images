5a235b82edc2ad14376dd6530dc02865ae5d5572
ownport <ownport@gmail.com>
2021-10-17 22:34:12 +0300

    test: tags count for repos list

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -4,15 +4,15 @@
 GITLAB_PROJECT_ID="23580555"
 
 # URLs
-URL_REPO_LIST="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/registry/repositories"
+URL_REPO_LIST="https://gitlab.com/api/v4/projects/${GITLAB_PROJECT_ID}/registry/repositories?tags_count=true"
 
 list_repos() {
 
     GROUP_NAME=${1:-}
 
-    curl --silent --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" \
-        ${URL_REPO_LIST} | \
-    jq -c ".[] | select(.name | contains(\"${GROUP_NAME}\"))"
+    curl --silent --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" \
+        ${URL_REPO_LIST} | jq . #\
+    # jq -c ".[] | select(.name | contains(\"${GROUP_NAME}\"))"
 }
 
 delete_repo() {
