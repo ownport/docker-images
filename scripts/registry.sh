697a94e88684ce9987056476e2dbd244147bb107
ownport <ownport@gmail.com>
2021-10-17 22:45:19 +0300

    test: invalid job token

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -10,9 +10,10 @@ list_repos() {
 
     GROUP_NAME=${1:-}
 
-    curl --silent --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" \
-        ${URL_REPO_LIST} | jq . #\
-    # jq -c ".[] | select(.name | contains(\"${GROUP_NAME}\"))"
+    curl --silent --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" \
+        ${URL_REPO_LIST} | \
+    jq . 
+    # -c ".[] | select(.name | contains(\"${GROUP_NAME}\"))"
 }
 
 delete_repo() {
