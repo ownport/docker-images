57fdd99f3226d4ab1ec8f42e3ecfe054d0b73563
ownport <ownport@gmail.com>
2021-10-17 21:45:33 +0300

    feat: format project list output

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -7,13 +7,14 @@ URL_PROJECT_IDS="https://gitlab.com/api/v4/projects/"
 
 list_projects() {
 
-    curl --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" ${URL_PROJECT_IDS}
+    curl --silent --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" \
+        ${URL_PROJECT_IDS} | jq .
 }
 
 list_repos() {
 
-    curl --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" ${URL_REPO_LIST}
-
+    curl --silent --header "PRIVATE-TOKEN: ${CU_JOB_TOKEN}" \
+        ${URL_REPO_LIST}
 }
 
 $@
