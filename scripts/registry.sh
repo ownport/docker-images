e62322c4e4adb780ec21f952ff13a6e35e7ffe8b
ownport <ownport@gmail.com>
2021-10-19 22:38:35 +0300

    test: use CI_REGISTRY_PASSWORD instead of CI_JOB_TOKEN

--- scripts/registry.sh
+++ scripts/registry.sh
@@ -10,7 +10,7 @@ list_repos() {
 
     GROUP_NAME=${1:-}
 
-    curl --silent --header "PRIVATE-TOKEN: ${CI_JOB_TOKEN}" \
+    curl --silent --header "PRIVATE-TOKEN: ${CI_REGISTRY_PASSWORD}" \
         ${URL_REPO_LIST} | \
     jq . 
     # -c ".[] | select(.name | contains(\"${GROUP_NAME}\"))"
