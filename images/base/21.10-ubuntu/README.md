aff1a45fff0e7b9c70cd781072495dc4d4661723
ownport <ownport@gmail.com>
2021-11-28 19:46:09 +0200

    feat: base:21.10-ubuntu

---
+++ images/base/21.10-ubuntu/README.md
@@ -0,0 +1,11 @@
+# Base | Ubuntu | 21.10
+
+## Tools
+
+### Deploy Utils
+
+Available options:
+- `update`: update Ubuntu packages
+- `install`: install packages
+- `install-build-deps`: install build deps
+- `cleanup`: cleanup
---
+++ images/base/21.10-ubuntu/assets/bin/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eu
+
+echo "[INFO] Install deploy-utils script" && \
+    mv /tmp/assets/bin/deploy-utils.sh /usr/local/bin/
+    
+echo "[INFO] Running smoke tests" && \
+    /tmp/assets/tests/smoke-tests.sh
+
+deploy-utils.sh cleanup
