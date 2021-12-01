60b8c37cf071c79aafebe56885da59d0cb6d85a1
ownport <ownport@gmail.com>
2021-12-01 12:20:43 +0200

    feat: add more logging and creating /kaniko/.docker directory from the script

--- images/kaniko/1.7-slim/assets/bin/update-docker-config.sh
+++ images/kaniko/1.7-slim/assets/bin/update-docker-config.sh
@@ -4,6 +4,10 @@ set -eu
 
 GITLAB_CREDS=$(echo -n ${CI_REGISTRY_USER}:${CI_REGISTRY_PASSWORD} | base64 | tr -d '\n')
 
+echo "[INFO] Creating directory, /kaniko/.docker" && \
+    mkdir -p /kaniko/.docker
+
+echo "[INFO] Updating kaniko docker config" && \
 echo "
 {
     \"auths\":{
