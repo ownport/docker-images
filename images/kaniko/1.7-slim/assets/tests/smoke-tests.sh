c3d2fd40f248106164411b41555a3e18418145fb
ownport <ownport@gmail.com>
2021-12-03 22:04:59 +0200

    fix: if condition in smoke tests

--- images/kaniko/1.7-slim/assets/tests/smoke-tests.sh
+++ images/kaniko/1.7-slim/assets/tests/smoke-tests.sh
@@ -8,14 +8,14 @@ echo "[TEST] Print out Kaniko version" && \
     /kaniko/executor version
 
 echo "[TEST] Checking the '/kaniko/update-docker-config.sh' script exists" && \
-    if ( ! -f /kaniko/update-docker-config.sh );
+    if [ ! -f /kaniko/update-docker-config.sh ];
     then
         echo "[ERROR] The file '/kaniko/update-docker-config.sh' does not exist"
         exit 1
     fi
 
 echo "[TEST] Checking the '/kaniko/update-docker-config.sh' script is executable" && \
-    if ( ! -x "/kaniko/update-docker-config.sh" );
+    if [ ! -x "/kaniko/update-docker-config.sh" ];
     then 
         echo "[ERROR] The file '/kaniko/update-docker-config.sh' does not executable"
         exit 1
