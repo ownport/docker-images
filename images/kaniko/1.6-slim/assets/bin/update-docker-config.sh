c9eb5bef1f92ae51722e7929f1bb9d9801c29cfc
ownport <ownport@gmail.com>
2021-12-03 19:50:43 +0200

    refact: create /kaniko/.docker dir

--- images/kaniko/1.6-slim/assets/bin/update-docker-config.sh
+++ images/kaniko/1.6-slim/assets/bin/update-docker-config.sh
@@ -4,6 +4,9 @@ set -eu
 
 GITLAB_CREDS=$(echo -n ${CI_REGISTRY_USER}:${CI_REGISTRY_PASSWORD} | base64 | tr -d '\n')
 
+echo "[INFO] Creating /kaniko/.docker/" && 
+    mkdir -p /kaniko/.docker/
+
 echo "
 {
     \"auths\":{
