f9f85c4f8ea87065125a084c4f19f76366956c67
ownport <ownport@gmail.com>
2021-12-05 18:11:49 +0000

    feature/120: migrate scraping images to new approach

--- images/scraping/scraping-base-0.1-alpine/assets/bin/install.sh
+++ images/scraping/scraping-base-0.1-alpine/assets/bin/install.sh
@@ -6,18 +6,14 @@ set -eu
 deploy-utils.sh update
 
 # Installation
-deploy-utils.sh install "bash jq"
-deploy-utils.sh install "libxml2 libxslt"
+deploy-utils.sh install "zsh jq"
+deploy-utils.sh install "py3-lxml py3-requests py3-beautifulsoup4 py3-yaml" 
+deploy-utils.sh install "py3-pytest py3-pytest-cov py3-pytest-xdist py3-pytest-benchmark"
 
-deploy-utils.sh install-build-deps "py3-wheel gcc musl-dev python3-dev build-base libxml2-dev libxslt-dev"
+py3-utils.sh install "parsel" 
 
-py3-utils.sh install "requests==2.24.0"
-py3-utils.sh install "lxml==4.5.2" 
-py3-utils.sh install "parsel==1.6.0" 
-py3-utils.sh install "beautifulsoup4==4.9.1"
-py3-utils.sh install "PyYAML==5.3.1" 
-
-py3-utils.sh install "pytest==5.4.3 pytest-cov==2.10.0 pytest-xdist==1.33.0 pytest-benchmark==3.2.3"
+echo "[INFO] Running smoke tests" && \
+    /tmp/assets/tests/smoke-tests.sh
 
 # Cleanup procedure
 deploy-utils.sh cleanup
--- images/scraping/scrapy-2.5-alpine/CHANGELOG.md
+++ images/scraping/scrapy-2.5-alpine/CHANGELOG.md
@@ -3,3 +3,4 @@
 ## scrapy:2.5-alpine
 
 - use python:3.9-alpine
+- scrapy:2.5.1
