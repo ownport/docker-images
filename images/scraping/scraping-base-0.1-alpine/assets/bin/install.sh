b7fdf6e752576be85125f39f225740e32bb31708
ownport <ownport@gmail.com>
2021-12-05 16:50:37 +0200

    refact: migrate scraping image to new approach

--- images/scraping/scraping-base-0.1-alpine/assets/bin/install.sh
+++ images/scraping/scraping-base-0.1-alpine/assets/bin/install.sh
@@ -6,7 +6,7 @@ set -eu
 deploy-utils.sh update
 
 # Installation
-deploy-utils.sh install "bash jq"
+deploy-utils.sh install "zsh jq"
 deploy-utils.sh install "libxml2 libxslt"
 
 deploy-utils.sh install-build-deps "py3-wheel gcc musl-dev python3-dev build-base libxml2-dev libxslt-dev"
@@ -18,6 +18,9 @@ py3-utils.sh install "beautifulsoup4==4.9.1"
 py3-utils.sh install "PyYAML==5.3.1" 
 
 py3-utils.sh install "pytest==5.4.3 pytest-cov==2.10.0 pytest-xdist==1.33.0 pytest-benchmark==3.2.3"
+
+echo "[INFO] Running smoke tests" && \
+    /tmp/assets/tests/smoke-tests.sh
 
 # Cleanup procedure
 deploy-utils.sh cleanup
